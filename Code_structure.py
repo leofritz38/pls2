7d816535c9904175bbf334f22779b62894630f2e
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-11-27 10:49:35 +0100

    ajout d'une structure de code

---
+++ Code_structure.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+import pandas as pd
+
+#### 1. Generate a vector of parameters
+c=[....]
+#### 2. Polynomial regression
+
+#### 3. Feature standardization
+
+#### 4. feature selection
+
+#### 5. ODE solution
+
+#### 6. error minimization
