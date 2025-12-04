f0f61f5efba2581cf07a77af1e84ac522b05f476
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-12-04 09:13:51 +0100

    changing the structure

--- Code_structure.py
+++ Code_structure.py
@@ -1,17 +1,4 @@
-import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
-from sklearn.pipeline import Pipeline
-import pandas as pd
-
-#### 1. Generate a vector of parameters
-c=[....]
-#### 2. Polynomial regression
-
-#### 3. Feature standardization
-
-#### 4. feature selection
-
-#### 5. ODE solution
-
-#### 6. error minimization
+# Define some models
+# Reading some dataframe
+# Optimizing some curve according to model optimization on data
+# Return a diagnostic
