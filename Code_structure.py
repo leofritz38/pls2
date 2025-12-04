c653fec3af70749222c95500da6c9c4fbfbffa58
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-12-04 09:25:27 +0100

    precising goals

--- Code_structure.py
+++ Code_structure.py
@@ -1,4 +1,27 @@
-# Define some models
-# Reading some dataframe
-# Optimizing some curve according to model optimization on data
-# Return a diagnostic
+#### Define some models
+# Goal : Having an extra file where we define all models from paper? Let's start with 2
+#
+#
+#
+#
+#
+#### Define some distances
+# Goal : Having an extra file where different distance calculation are defined
+#
+#
+#
+#
+#
+#### Reading some dataframe
+# Goal : extracting and organize all our datas
+#
+#
+#
+#
+#### Optimizing some curve according to model optimization on data
+# Goal : function that given ETR datas, model/models, distance/distances
+# 
+# 
+#  
+#### Return a diagnostic
+# Stress / lack of something
