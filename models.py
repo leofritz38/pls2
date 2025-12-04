305d6b2903dde384860ad4cb584536bfeb429c23
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-12-04 10:19:26 +0100

    adding models 1,2,3,4,6

--- models.py
+++ models.py
@@ -1,22 +1,22 @@
 ##### The goal of this file is to define as many models as wanted to use for optimization
-import math
+import numpy as np 
 def Model1(E,alpha,ETRmax):
     return((ETRmax*alpha*E)/((ETRmax*alpha)+E))
 def Model2(E,alpha,ETRmax):
-    return(ETRmax*(1-math.exp(-(alpha*E)/ETRmax)))
+    return(ETRmax*(1-np.exp(-(alpha*E)/ETRmax)))
 def Model3(E,alpha,ETRmax):
-    return(ETRmax*math.tanh((alpha*E)/ETRmax))
-def Model4(E,alpha,ETRmax):
-    return()
-def Model5(E,alpha,ETRmax):
-    return()
+    return(ETRmax*np.tanh((alpha*E)/ETRmax))
+def Model4(E,alpha,ETRmax,m):
+    return((ETRmax*E)/((ETRmax/alpha)**m+E**m)**(1/m))
+# def Model5(E,alpha,ETRmax):
+#     return()
 def Model6(E,alpha,ETRmax):
-    return()
-def Model7(E,alpha,ETRmax):
-    return()
-def Model8(E,alpha,ETRmax):
-    return()
-def Model9(E,alpha,ETRmax):
-    return()
-def Model10(E,alpha,ETRmax):
-    return()
+    return(alpha*E*np.exp(-((alpha*E)/(ETRmax*np.exp(1)))))
+# def Model7(E,alpha,ETRmax):
+#     return()
+# #def Model8(E,alpha,ETRmax):
+# #    return()
+# #def Model9(E,alpha,ETRmax):
+#     return()
+# #def Model10(E,alpha,ETRmax):
+#     return()
