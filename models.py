d39ab0423df93ed47226b6303ffb951f7fb8eff2
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-12-07 19:10:39 +0100

    changing model synthaxe

--- models.py
+++ models.py
@@ -1,16 +1,32 @@
 ##### The goal of this file is to define as many models as wanted to use for optimization
 import numpy as np 
-def Model1(E,alpha,ETRmax):
+def Model1(E,param=["ETRmax","alpha"]):
+    alpha=param[0]
+    ETRmax=param[1]
+    E = np.atleast_1d(E)
     return((ETRmax*alpha*E)/((ETRmax*alpha)+E))
-def Model2(E,alpha,ETRmax):
+def Model2(E,param=["ETRmax","alpha"]):
+    alpha=param[0]
+    ETRmax=param[1]
+    E = np.atleast_1d(E)
     return(ETRmax*(1-np.exp(-(alpha*E)/ETRmax)))
-def Model3(E,alpha,ETRmax):
+def Model3(E,param=["ETRmax","alpha"]):
+    alpha=param[0]
+    ETRmax=param[1]
+    E = np.atleast_1d(E)
     return(ETRmax*np.tanh((alpha*E)/ETRmax))
-def Model4(E,alpha,ETRmax,m):
+def Model4(E,param=["ETRmax","alpha","m"]):
+    alpha=param[0]
+    ETRmax=param[1]
+    m=param[2]
+    E = np.atleast_1d(E)
     return((ETRmax*E)/((ETRmax/alpha)**m+E**m)**(1/m))
 # def Model5(E,alpha,ETRmax):
 #     return()
-def Model6(E,alpha,ETRmax):
+def Model6(E,param=["ETRmax","alpha"]):
+    alpha=param[0]
+    ETRmax=param[1]
+    E = np.atleast_1d(E)
     return(alpha*E*np.exp(-((alpha*E)/(ETRmax*np.exp(1)))))
 # def Model7(E,alpha,ETRmax):
 #     return()
