85f95576d8b1cf5439506437eb8c32bf328f4f98
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-12-07 22:08:02 +0100

    adding bis distance to test multiple distances application

--- distances.py
+++ distances.py
@@ -6,5 +6,10 @@ def least_square_distance(param,model,x,y_ref):
     y_pred=model(x,param)
     dist=sum((y_ref-y_pred)**2)
     return dist
+def least_square_distance2(param,model,x,y_ref):
+    # current_mod=getattr(models,model)
+    y_pred=model(x,param)
+    dist=sum((y_ref-y_pred)**2)
+    return dist
 
 
