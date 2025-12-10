ea01fda9249b64ac6f01d8c1e391e0c8c5bc1faa
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-12-10 17:59:05 +0100

    modifying distance -> take whole dataframe and variable specific

--- distances.py
+++ distances.py
@@ -1,23 +1,27 @@
 import numpy as np
 
 
-def least_square_distance(param,model,x,y_ref):
+def least_square_distance_ETR(param,model,data):
     # current_mod=getattr(models,model)
+    x=data["PAR"]
+    y_ref=data["ETR"]
     y_pred=model(x,param)
     dist=sum((y_ref-y_pred)**2)
     return dist
-def least_square_distance2(param,model,x,y_ref):
+def least_square_distance2(param,model,data):
     # current_mod=getattr(models,model)
+    x=data["PAR"]
+    y_ref=data["ETR"]
     y_pred=model(x,param)
     dist=sum((y_ref-y_pred)**2)
     return dist
-def merge_distance(param,model,x,y_ref,dist_to_merge,ponderation):
+def merge_distance(param,model,data,dist_to_merge,ponderation):
     if len(ponderation)!=len(dist_to_merge):
         raise ValueError("Must be the same amount of distances to merge and ponderation")
     dist=0
     for i in range(0,len(dist_to_merge)):
         current_dist=globals()[dist_to_merge[i]]
-        dist_value=ponderation[i]*current_dist(param,model,x,y_ref)
+        dist_value=ponderation[i]*current_dist(param,model,data)
         dist+=dist_value
     dist=dist
     return dist
