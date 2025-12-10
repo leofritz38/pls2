d650681b1ef4a357b16b876fcb5634c60377ea6e
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-12-10 16:10:33 +0100

    adding a function that return weighted distance for any number of distances

--- distances.py
+++ distances.py
@@ -11,5 +11,17 @@ def least_square_distance2(param,model,x,y_ref):
     y_pred=model(x,param)
     dist=sum((y_ref-y_pred)**2)
     return dist
+def merge_distance(param,model,x,y_ref,dist_to_merge,ponderation):
+    if len(ponderation)!=len(dist_to_merge):
+        raise ValueError("Must be the same amount of distances to merge and ponderation")
+    dist=0
+    for i in range(0,len(dist_to_merge)):
+        current_dist=globals()[dist_to_merge[i]]
+        dist_value=ponderation[i]*current_dist(param,model,x,y_ref)
+        dist+=dist_value
+    dist=dist
+    return dist
+
+
 
 
