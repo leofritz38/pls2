8a2e90dbe4906d3571bc89e7e538bb2bfe09687d
Leo FRITZ <fritz.leo.benoit@gmail.com>
2025-12-10 20:05:34 +0100

    fixing bug in parameters order

--- models.py
+++ models.py
@@ -1,38 +1,38 @@
 ##### The goal of this file is to define as many models as wanted to use for optimization
 import numpy as np 
-def Model1(E,param=["ETRmax","alpha"]):
+def Model1(E,param=["alpha","ETR"]):
     alpha=param[0]
-    ETRmax=param[1]
+    ETR=param[1]
     E = np.atleast_1d(E)
-    return((ETRmax*alpha*E)/((ETRmax*alpha)+E))
-def Model2(E,param=["ETRmax","alpha"]):
+    return((ETR*alpha*E)/((ETR*alpha)+E))
+def Model2(E,param=["alpha","ETR"]):
     alpha=param[0]
-    ETRmax=param[1]
+    ETR=param[1]
     E = np.atleast_1d(E)
-    return(ETRmax*(1-np.exp(-(alpha*E)/ETRmax)))
-def Model3(E,param=["ETRmax","alpha"]):
+    return(ETR*(1-np.exp(-(alpha*E)/ETR)))
+def Model3(E,param=["alpha","ETR"]):
     alpha=param[0]
-    ETRmax=param[1]
+    ETR=param[1]
     E = np.atleast_1d(E)
-    return(ETRmax*np.tanh((alpha*E)/ETRmax))
-def Model4(E,param=["ETRmax","alpha","m"]):
+    return(ETR*np.tanh((alpha*E)/ETR))
+def Model4(E,param=["alpha","ETR","m"]):
     alpha=param[0]
-    ETRmax=param[1]
+    ETR=param[1]
     m=param[2]
     E = np.atleast_1d(E)
-    return((ETRmax*E)/((ETRmax/alpha)**m+E**m)**(1/m))
-# def Model5(E,alpha,ETRmax):
+    return((ETR*E)/((ETR/alpha)**m+E**m)**(1/m))
+# def Model5(E,alpha,ETR):
 #     return()
-def Model6(E,param=["ETRmax","alpha"]):
+def Model6(E,param=["alpha","ETR"]):
     alpha=param[0]
-    ETRmax=param[1]
+    ETR=param[1]
     E = np.atleast_1d(E)
-    return(alpha*E*np.exp(-((alpha*E)/(ETRmax*np.exp(1)))))
-# def Model7(E,alpha,ETRmax):
+    return(alpha*E*np.exp(-((alpha*E)/(ETR*np.exp(1)))))
+# def Model7(E,alpha,ETR):
 #     return()
-# #def Model8(E,alpha,ETRmax):
+# #def Model8(E,alpha,ETR):
 # #    return()
-# #def Model9(E,alpha,ETRmax):
+# #def Model9(E,alpha,ETR):
 #     return()
-# #def Model10(E,alpha,ETRmax):
+# #def Model10(E,alpha,ETR):
 #     return()
