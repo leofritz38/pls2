e874f7e85c926f953b898b919a3ebe04f0ad0eb5
QUENTIN-FAYE <faye.quentin11@gmail.com>
2025-12-12 10:28:05 +0100

    created a function  that take as sys arg[1] the csv file path and that gives back a pandas Dataframe

--- data_loading.py
+++ data_loading.py
@@ -5,12 +5,15 @@ from sys import argv
 
 print("data need to be extracted fom file.zip in order ot be readen by read_csv")
 print(" give the data_file pathway with / not \ ")
-file_name=argv[1]
+
 #file_name="data_algae/Online_PAM/00_process_data/00_rlc_data/00_5s/00_pam1/data_PAM_LR_070_batch_G1_MI5_MEA10_5s_PAM1.csv"
 
-
-
-data=pandas.read_csv(file_name, sep=";", decimal=".")
-print(data.head(), "all the data files")
-data_interst=data.loc[:,["PAR","ETR","rETR","F","Fm'","Y(II)"]]
-print(data_interst.head(),"only the data of interest")
+def file_downloader(file_name):
+    data=pandas.read_csv(file_name, sep=";", decimal=".")
+#print(data.head(), "all the data files")
+    data_interst=data.loc[:,["PAR","ETR","rETR","F","Fm'","Y(II)"]] 
+    print(data_interst.head(),"only the data of interest")
+    
+    return (data_interst)
+file_nameT=argv[1]
+data_algea=file_downloader(file_name=file_nameT)
