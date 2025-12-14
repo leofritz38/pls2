689658eb32c59c007a5ca84b3aa0b7a20234c237
QUENTIN-FAYE <faye.quentin11@gmail.com>
2025-12-14 18:54:09 +0100

    keep downloding function and comment the code

--- interpret_data.py
+++ interpret_data.py
@@ -1,6 +1,6 @@
 #interpret_data
 
-from data_loading import file_downloader
+
 import models
 import optimizer
 import distances
@@ -10,50 +10,65 @@ import matplotlib.pyplot as plt
 from models import Model2 # to be removed may be
 import pandas
 
+
+
+def file_downloader(file_name):
+    data=pandas.read_csv(file_name, sep=";", decimal=".")
+    # print(data.head(), "all the data files")
+    data_interst=data.loc[:,["t in s","PAR","ETR","rETR","F","Fm'","Y(II)"]] 
+    print(data_interst.head(),"only the data of interest")
+    
+    return (data_interst)
+
 # ---------------data downloading-----------
-file_nameT=argv[1]
-data_algea=file_downloader(file_name=file_nameT)
+file_nameT=argv[1] # donner dans la batch de commande le nom et le paths pour acceder au donnée
+data_algea=file_downloader(file_name=file_nameT) # utiliser la fonction qui prend le csv en donnée et donnes un dataframe en sortie
 
 # -----------------------data exploitation with some MODELs and some DISTANCES computing
-used_dist=["least_square_distance","least_square_distance2"]
+used_dist=["least_square_distance","least_square_distance2"] # selection des distances utilisé pour l'optimisagtion
 
-optimized_parameters=optimizer.optimize_models(modname=["Model1","Model2"],dist=used_dist,rawdata=data_algea) 
-best_optimization_per_distance=optimizer.get_best_prediction(optimized_parameters)
-print(best_optimization_per_distance)
+optimized_parameters=optimizer.optimize_models(modname=["Model1","Model2"],dist=used_dist,rawdata=data_algea) # donne les paramètre optimisé sur tous les jeux de données
+# quand la fonction optimizer etait construire pour obtenir des parameter sur tout le jeux de données . On s'est rendu compte plustard que la structrue du jeux de données
+#etait : un individus stat egal 10 mesures
+best_optimization_per_distance=optimizer.get_best_prediction(optimized_parameters) #  fonction qui avec les paramètres optimisé donnes le meilleurs modèle pour cahque distance selectioné 
+print(best_optimization_per_distance) # affiche le meilleur modèle avec la distances moyennes associé ? lorsque la structure des données n'etais pas prise en compte par optimizer
+# les ecart entre predictions et mesure etait tres grand 
 # print(optimized_parameters)
 # plot it now---------------------------------------------------
 # Extraire ETRmax et alpa
-ETRmax =float(optimized_parameters['Model2']['least_square_distance']['optimized_parameters']['ETRmax'])
+ETRmax =float(optimized_parameters['Model2']['least_square_distance']['optimized_parameters']['ETRmax']) # recurper les paramètres optimisé pour le modèle 2
 alpha = float(optimized_parameters['Model2']['least_square_distance']['optimized_parameters']['alpha']) 
 
-print(alpha, "alpha")
-print(type(alpha), "alpha type")
-rETR_easy=data_algea["rETR"].values
-E=data_algea["PAR"].values
+print(alpha, "alpha")  # afficher le paramètre de croissance pour voir si il etait coherent, av prise en compte de la structure des données il ne l'etait pas
 
 
-parameters = [ ETRmax, alpha]
-rETR_model=Model2(E,param=parameters)
-print(rETR_model[:10], "les 5 premières valeurs de rETR_model")
+rETR_easy=data_algea["rETR"].values # recuprere les données qui doivent etre simulé par modèle 2
+E=data_algea["PAR"].values # recuprer le X a mettre dans notrmodèle2 qui avec les paramètre nous renvoie notre Y : rETR
+
+
+parameters = [ ETRmax, alpha] # paramètre precedement extrait de optimizer
+rETR_model=Model2(E,param=parameters) # compute rETr modèle 2
+
+print(rETR_model[:10], "les 5 premières valeurs de rETR_model") # visualiser les deux, encore une fois avnt la prise en compte de la structurations des donnees il y avait un grosse ecart
 print(rETR_easy[:10], "les 5 premières valeurs de rETR_easy")
 
-# # firugre -----------------------------------
-# plt.figure()  # Crée une nouvelle figure vide
-# plt.plot(rETR_easy, rETR_model )  #
-# plt.xlabel('ETR from simpler model')
-# plt.ylabel('ETR from model to get coefficient')
-# plt.title('peformance du meilleur modèle a reproduire les données')
-# plt.grid(True)
-# plt.show()
+# firugre ----------------------------------- plot Y en fonction X : plot rETR estimé par rapport  rETR mesuré
+plt.figure()  # Crée une nouvelle figure vide
+plt.plot(rETR_easy, rETR_model )  #
+plt.xlabel('ETR from simpler model')
+plt.ylabel('ETR from model to get coefficient')
+plt.title('peformance du meilleur modèle a reproduire les données')
+plt.grid(True)
+plt.show()
 
 
-# --------------representer alpha en fonction du temps ? -----------
+# --------------representer alpha en fonction du temps ? ----------- meme code avant mais bouvle For pour le faire sur tout le scripte
 # creer element pour la figure de alpha en fonction du temps et alpha en fonction du ph 
 # obtenir alpha 
-# travaillans colomne par colomne 
+# travaillans colomne par colomne  -> pas possible à partir de comment opimizer etait construit
 # ajoute un colomne   alpha
-alphas = []
-window_size = 5
+alphas = [] # creer un vecteur vide
+window_size = 5 # creer une fenetre de donnnes avec lesquelles les paramètres seront optimisées
 
 window_data=data_algea.iloc[0:30] # ligne ou colomne
 print(window_data, " premier lignes toutes les colomnes ? ")
@@ -62,12 +77,12 @@ result = optimizer.optimize_models(
         modname=["Model2"],
         dist=used_dist,
         rawdata=window_data
-    )
+    ) # compute parameter value only for the data in the window
 
-alpha=result['Model2']['least_square_distance']['optimized_parameters']['alpha']
-print(alpha, " alpha singulier")
-alphas.append(alpha)
-print(alphas , "vecteur alphaS")
+alpha=result['Model2']['least_square_distance']['optimized_parameters']['alpha']  # retreive paramèter value only for the modele and distance of intereste
+print(alpha, " alpha singulier") # afficher la croissance pour voir si c'est coherant avec ce qu'on attends
+alphas.append(alpha) # ajouter au vecteur nul
+
 print(Model2(E=window_data["PAR"].values, param=[ETRmax,alpha]), "rETR estimé pour un alpha local d 'une fenetre de 5 ")
 
 # for i in range(len(data_algea)):
